96855b7acaca4ece36015b26770eef140f533fd6
Deepan Kumar <145337793+dipankumar1098@users.noreply.github.com>
2024-06-19 18:37:15 +0530

    java

---
+++ Tablef.java
@@ -0,0 +1,15 @@
+class Table{
+	public static void main(String []args){
+				int num=5;
+                int i =1;
+		for(i=1;i<=10;i++){
+		System.out.printf("%d * %d = %d \n",num,i,num*i);
+	}
+}
+}
+		
+		
+	
+		
+	
+	
